28c442ed0e0df85cf3ddafc3b968656e83ddf032
pei jason <pjd@live.cn>
2019-06-22 11:58:38 +0900

    add sample code

--- core-java/src/test/java/com/ripjava/java/core/optional/OptionalTest.java
+++ core-java/src/test/java/com/ripjava/java/core/optional/OptionalTest.java
@@ -2,7 +2,12 @@ package com.ripjava.java.core.optional;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +25,7 @@ public class OptionalTest {
         Optional<String> opt = Optional.of(name);
         assertTrue(opt.isPresent());
     }
+
     @Test
     public void givenNull_whenThrowsErrorOnCreate_thenCorrect() {
         String name = null;
@@ -27,9 +33,296 @@ public class OptionalTest {
     }
 
     @Test
-    public void givenNonNull_whenCreatesNullable_thenCorrect() {
+    public void givenNull_whenCreatesNullable_thenCorrect() {
         String name = null;
         Optional<String> opt = Optional.ofNullable(name);
         assertFalse(opt.isPresent());
     }
+
+    @Test
+    public void givenNonNull_whenCreatesNullable_thenCorrect() {
+        String name = "ripjava";
+        Optional<String> opt = Optional.ofNullable(name);
+        assertTrue(opt.isPresent());
+    }
+
+    @Test
+    public void givenOptional_whenIsPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("ripjava");
+        assertTrue(opt.isPresent());
+
+        opt = Optional.ofNullable(null);
+        assertFalse(opt.isPresent());
+    }
+
+    @Test
+    public void givenAnEmptyOptional_thenIsEmptyBehavesAsExpected() {
+        Optional<String> opt = Optional.of("ripjava");
+        assertFalse(opt.isEmpty());
+
+        opt = Optional.ofNullable(null);
+        assertTrue(opt.isEmpty());
+    }
+
+    @Test
+    public void givenOptional_whenIfPresentWorks_thenCorrect() {
+        Optional<String> opt = Optional.of("ripjava");
+        opt.ifPresent(name -> System.out.println(name.length()));
+    }
+
+    @Test
+    public void whenOrElseWorks_thenCorrect() {
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElse("test");
+        assertEquals("test", name);
+    }
+
+    @Test
+    public void whenOrElseGetWorks_thenCorrect() {
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElseGet(() -> "test");
+        assertEquals("test", name);
+    }
+
+    @Test
+    public void whenOrElseGetAndOrElseOverlap_thenCorrect() {
+        String text = null;
+
+        String defaultText = Optional.ofNullable(text).orElseGet(this::getMyDefault);
+        assertEquals("Default Value", defaultText);
+
+        defaultText = Optional.ofNullable(text).orElse(getMyDefault());
+        assertEquals("Default Value", defaultText);
+    }
+
+    public String getMyDefault() {
+        System.out.println("Getting Default Value");
+        return "Default Value";
+    }
+
+    @Test
+    public void whenOrElseGetAndOrElseDiffer_thenCorrect() {
+        String text = "Text present";
+
+        System.out.println("Using orElseGet:");
+        String defaultText
+                = Optional.ofNullable(text).orElseGet(this::getMyDefault);
+        assertEquals("Text present", defaultText);
+
+        System.out.println("Using orElse:");
+        defaultText = Optional.ofNullable(text).orElse(getMyDefault());
+        assertEquals("Text present", defaultText);
+    }
+
+    @Test
+    public void whenOrElseThrowWorks_thenCorrect() {
+        String nullName = null;
+        assertThrows(IllegalArgumentException.class, () -> Optional.ofNullable(nullName).orElseThrow(
+                IllegalArgumentException::new));
+    }
+
+    @Test
+    public void givenOptional_whenGetsValue_thenCorrect() {
+        Optional<String> opt = Optional.of("ripjava");
+        String name = opt.get();
+        assertEquals("ripjava", name);
+    }
+
+    @Test
+    public void givenOptionalWithNull_whenGetThrowsException_thenCorrect() {
+        Optional<String> opt = Optional.ofNullable(null);
+        assertThrows(NoSuchElementException.class, () -> opt.get());
+    }
+
+    @Test
+    public void whenOptionalFilterWorks_thenCorrect() {
+        Integer year = 2016;
+        Optional<Integer> yearOptional = Optional.of(year);
+        boolean is2016 = yearOptional.filter(y -> y == 2016).isPresent();
+        assertTrue(is2016);
+        boolean is2020 = yearOptional.filter(y -> y == 2020).isPresent();
+        assertFalse(is2020);
+    }
+
+    public class Modem {
+        private Double price;
+
+        public Modem(Double price) {
+            this.price = price;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+    }
+    public boolean priceIsInRange1(Modem modem) {
+        boolean isInRange = false;
+
+        if (modem != null && modem.getPrice() != null
+                && (modem.getPrice() >= 10
+                && modem.getPrice() <= 15)) {
+
+            isInRange = true;
+        }
+        return isInRange;
+    }
+    @Test
+    public void whenFiltersWithoutOptional_thenCorrect() {
+        assertTrue(priceIsInRange1(new Modem(10.0)));
+        assertFalse(priceIsInRange1(new Modem(9.9)));
+        assertFalse(priceIsInRange1(new Modem(null)));
+        assertFalse(priceIsInRange1(new Modem(15.5)));
+        assertFalse(priceIsInRange1(null));
+    }
+
+    public boolean priceIsInRange2(Modem modem2) {
+        return Optional.ofNullable(modem2)
+                .map(Modem::getPrice)
+                .filter(p -> p >= 10)
+                .filter(p -> p <= 15)
+                .isPresent();
+    }
+
+    @Test
+    public void whenFiltersWithOptional_thenCorrect() {
+        assertTrue(priceIsInRange2(new Modem(10.0)));
+        assertFalse(priceIsInRange2(new Modem(9.9)));
+        assertFalse(priceIsInRange2(new Modem(null)));
+        assertFalse(priceIsInRange2(new Modem(15.5)));
+        assertFalse(priceIsInRange2(null));
+    }
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect() {
+        List<String> companyNames = Arrays.asList(
+                "alibaba", "oracle", "", "microsoft", "", "apple");
+        Optional<List<String>> listOptional = Optional.of(companyNames);
+
+        int size = listOptional
+                .map(List::size)
+                .orElse(0);
+        assertEquals(6, size);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect2() {
+        String name = "ripjava";
+        Optional<String> nameOptional = Optional.of(name);
+
+        int len = nameOptional
+                .map(String::length)
+                .orElse(0);
+        assertEquals(7, len);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorksWithFilter_thenCorrect() {
+        String password = " password ";
+        Optional<String> passOpt = Optional.of(password);
+        boolean correctPassword = passOpt.filter(
+                pass -> pass.equals("password")).isPresent();
+        assertFalse(correctPassword);
+
+        correctPassword = passOpt
+                .map(String::trim)
+                .filter(pass -> pass.equals("password"))
+                .isPresent();
+        assertTrue(correctPassword);
+    }
+
+    @Test
+    public void givenOptional_whenFlatMapWorks_thenCorrect2() {
+        Person person = new Person("john", 26);
+        Optional<Person> personOptional = Optional.of(person);
+
+        Optional<Optional<String>> nameOptionalWrapper
+                = personOptional.map(Person::getName);
+        Optional<String> nameOptional
+                = nameOptionalWrapper.orElseThrow(IllegalArgumentException::new);
+        String name1 = nameOptional.orElse("");
+        assertEquals("john", name1);
+
+        String name = personOptional
+                .flatMap(Person::getName)
+                .orElse("");
+        assertEquals("john", name);
+    }
+
+
+
+    private Optional<String> getEmpty() {
+        return Optional.empty();
+    }
+
+    private Optional<String> getHello() {
+        return Optional.of("hello");
+    }
+
+    private Optional<String> getBye() {
+        return Optional.of("bye");
+    }
+
+    private Optional<String> createOptional(String input) {
+        if (input == null || "".equals(input) || "empty".equals(input)) {
+            return Optional.empty();
+        }
+        return Optional.of(input);
+    }
+    @Test
+    public void givenThreeOptionals_whenChaining_thenFirstNonEmptyIsReturned() {
+        Optional<String> found = Stream.of(getEmpty(), getHello(), getBye())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+
+        assertEquals(getHello(), found);
+    }
+
+    @Test
+    public void givenThreeOptionals_whenChaining_thenFirstNonEmptyIsReturnedAndRestNotEvaluated() {
+        Optional<String> found =
+                Stream.<Supplier<Optional<String>>>of(this::getEmpty, this::getHello, this::getBye)
+                        .map(Supplier::get)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .findFirst();
+
+        assertEquals(getHello(), found);
+    }
+
+
+    @Test
+    public void givenTwoOptionalsReturnedByOneArgMethod_whenChaining_thenFirstNonEmptyIsReturned() {
+        Optional<String> found = Stream.<Supplier<Optional<String>>>of(
+                () -> createOptional("empty"),
+                () -> createOptional("hello")
+        )
+                .map(Supplier::get)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+
+        assertEquals(createOptional("hello"), found);
+    }
+
+
+    @Test
+    public void givenTwoEmptyOptionals_whenChaining_thenDefaultIsReturned() {
+        String found = Stream.<Supplier<Optional<String>>>of(
+                () -> createOptional("empty"),
+                () -> createOptional("empty")
+        )
+                .map(Supplier::get)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .orElseGet(() -> "default");
+
+        assertEquals("default", found);
+    }
+
 }
