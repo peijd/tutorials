a3f566854c369f94bc9ca151f83a70fc848e396b
pei jason <pjd@live.cn>
2019-07-17 21:01:24 +0900

    add sample code for Java regex2

--- core-java/src/test/java/com/ripjava/java/core/regex/RegexTest.java
+++ core-java/src/test/java/com/ripjava/java/core/regex/RegexTest.java
@@ -330,11 +330,216 @@ public class RegexTest {
         assertTrue(matches2 > 0);
     }
 
+    @Test
+    public void  test_RegexWithoutCanonEqMatchFailsOnEquivalentUnicode() {
+        int matches = runTest("\u00E9", "\u0065\u0301");
+
+        assertFalse(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithCanonEqMatchesOnEquivalentUnicode() {
+        int matches = runTest("\u00E9", "\u0065\u0301", Pattern.CANON_EQ);
+
+        assertTrue(matches > 0);
+    }
+
+
+    @Test
+    public void test_RegexWithDefaultMatcherMatchFailsOnDifferentCases() {
+        int matches = runTest("dog", "This is a Dog");
+
+        assertFalse(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithCaseInsensitiveMatcherMatchesOnDifferentCases() {
+        int matches = runTest(
+                "dog", "This is a Dog", Pattern.CASE_INSENSITIVE);
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithEmbeddedCaseInsensitiveMatcherMatchesOnDifferentCases() {
+        int matches = runTest("(?i)dog", "This is a Dog");
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithCommentsMatchFailsWithoutFlag() {
+        int matches = runTest(
+                "dog$  #check for word dog at end of text", "This is a dog");
+
+        assertFalse(matches > 0);
+    }
+    @Test
+    public void test_RegexWithCommentsMatchesWithFlag() {
+        int matches = runTest(
+                "dog$  #check end of text","This is a dog", Pattern.COMMENTS);
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithCommentsMatchesWithEmbeddedFlag() {
+        int matches = runTest(
+                "(?x)dog$  #check end of text", "This is a dog");
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithLineTerminatorMatchFails() {
+        Pattern pattern = Pattern.compile("(.*)");
+        Matcher matcher = pattern.matcher(
+                "this is a text" + System.getProperty("line.separator")
+                        + " continued on another line");
+        matcher.find();
+
+        assertEquals("this is a text", matcher.group(1));
+    }
+
+
+
+    @Test
+    public void test_RegexMatchesWithoutLiteralFlag() {
+        int matches = runTest("(.*)", "text");
+
+        assertTrue(matches > 0);
+    }
+    @Test
+    public void test_RegexMatchFailsWithLiteralFlag() {
+        int matches = runTest("(.*)", "text", Pattern.LITERAL);
+
+        assertFalse(matches > 0);
+    }
+
+    @Test
+    public void test_RegexMatchesWithLiteralFlag() {
+        int matches = runTest("(.*)", "text(.*)", Pattern.LITERAL);
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexMatchFailsWithoutMultilineFlag() {
+        int matches = runTest(
+                "dog$", "This is a dog" + System.getProperty("line.separator")
+                        + "this is a fox");
+
+        assertFalse(matches > 0);
+    }
+
+    @Test
+    public void test_RegexMatchesWithMultilineFlag() {
+        int matches = runTest(
+                "dog$", "This is a dog" + System.getProperty("line.separator")
+                        + "this is a fox", Pattern.MULTILINE);
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_Regex_whenMatchesWithEmbeddedMultilineFlag() {
+        int matches = runTest(
+                "(?m)dog$", "This is a dog" + System.getProperty("line.separator")
+                        + "this is a fox");
+
+        assertTrue(matches > 0);
+    }
+
+    @Test
+    public void test_RegexWithLineTerminatorMatchesWithDotall() {
+        Pattern pattern = Pattern.compile("(.*)", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(
+                "this is a text" + System.getProperty("line.separator")
+                        + " continued on another line");
+        matcher.find();
+        assertEquals(
+                "this is a text" + System.getProperty("line.separator")
+                        + " continued on another line", matcher.group(1));
+    }
+
+    @Test
+    public void test_RegexWithLineTerminatorMatchesWithEmbeddedDotall() {
+
+        Pattern pattern = Pattern.compile("(?s)(.*)");
+        Matcher matcher = pattern.matcher(
+                "this is a text" + System.getProperty("line.separator")
+                        + " continued on another line");
+        matcher.find();
+
+        assertEquals(
+                "this is a text" + System.getProperty("line.separator")
+                        + " continued on another line", matcher.group(1));
+    }
+
+
+    @Test
+    public void test_MatchGetsIndices() {
+        Pattern pattern = Pattern.compile("dog");
+        Matcher matcher = pattern.matcher("This dog is mine");
+        matcher.find();
+
+        assertEquals(5, matcher.start());
+        assertEquals(8, matcher.end());
+    }
+
+    @Test
+    public void test_StudyMethodsWork() {
+        Pattern pattern = Pattern.compile("dog");
+        Matcher matcher = pattern.matcher("dogs are friendly");
+
+        assertTrue(matcher.lookingAt());
+        assertFalse(matcher.matches());
+    }
+
+    @Test
+    public void test_StudyMethodWork2() {
+        Pattern pattern = Pattern.compile("dog");
+        Matcher matcher = pattern.matcher("dog");
+
+        assertTrue(matcher.matches());
+    }
+
+    @Test
+    public void test_ReplaceFirstWorks() {
+        Pattern pattern = Pattern.compile("dog");
+        Matcher matcher = pattern.matcher(
+                "dogs are domestic animals, dogs are friendly");
+        String newStr = matcher.replaceFirst("cat");
+
+        assertEquals(
+                "cats are domestic animals, dogs are friendly", newStr);
+    }
+
+    @Test
+    public void test_ReplaceAllWorks() {
+        Pattern pattern = Pattern.compile("dog");
+        Matcher matcher = pattern.matcher(
+                "dogs are domestic animals, dogs are friendly");
+        String newStr = matcher.replaceAll("cat");
+
+        assertEquals("cats are domestic animals, cats are friendly", newStr);
+    }
+
     public static int runTest(String regex, String text) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         int matches = 0;
         while (matcher.find()) {
+            matches++;
+        }
+        return matches;
+    }
+
+    public static int runTest(String regex, String text, int flags) {
+        Pattern pattern = Pattern.compile(regex, flags);
+        Matcher matcher = pattern.matcher(text);
+        int matches = 0;
+        while (matcher.find()){
             matches++;
         }
         return matches;
