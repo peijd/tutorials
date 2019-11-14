f2cfe4f9cb96a572146a053b2953eb721739fa6b
peijd <pjd@live.cn>
2019-11-14 22:53:20 +0900

    add spring mvc

---
+++ spring-mvc/src/main/java/com/ripjava/spring/mvc/config/MainWebAppInitializer.java
@@ -0,0 +1,27 @@
+package com.ripjava.spring.mvc.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MainWebAppInitializer implements WebApplicationInitializer {
+    public void onStartup(final ServletContext sc) throws ServletException {
+
+        AnnotationConfigWebApplicationContext root =
+                new AnnotationConfigWebApplicationContext();
+
+        root.scan("com.ripjava.spring.mvc");
+        sc.addListener(new ContextLoaderListener(root));
+
+        ServletRegistration.Dynamic appServlet =
+                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
+    }
+}
---
+++ spring-mvc/src/main/webapp/WEB-INF/view/sample.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jundongpei
+  Date: 2019/11/14
+  Time: 10:24 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>sample</title>
+</head>
+<body>
+<h1>This is the body of the sample view</h1>
+</body>
+</html>
