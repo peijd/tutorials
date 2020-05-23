8a1702bd76a2158479aa621be6c989212505431f
peijd <pjd@live.cn>
2020-05-23 23:13:29 +0900

    add maven Tutorial

---
+++ maven/parent-project/core/src/main/java/com/ripjava/App.java
@@ -0,0 +1,13 @@
+package com.ripjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
