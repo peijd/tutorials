16335406cd46a9ba0b19f4401a11f412970b7fd8
peijd <pjd@live.cn>
2020-05-31 17:47:13 +0900

    add maven find unsed dependenices

---
+++ maven/find-unused/src/main/java/com/ripjava/maven/UnusedDependenciesExample.java
@@ -0,0 +1,11 @@
+package com.ripjava.maven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnusedDependenciesExample {
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(UnusedDependenciesExample.class);
+
+    }
+}
